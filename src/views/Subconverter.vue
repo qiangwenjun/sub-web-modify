098c62b6002e24baf62c4afab33fbf50ea0f0e71
qiangwenjun <57254767+qiangwenjun@users.noreply.github.com>
2025-08-08 08:08:19 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,21 +339,9 @@ export default {
         },
         customBackend: {
           "爪云自建后端": "https://subapi.178557689.xyz",
-          #"CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
-          #"肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          #"肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          #nameless13提供: "https://www.nameless13.com",
-          #subconverter作者提供: "https://sub.xeton.dev",
-          #"sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
           { value: "https://subapi.178557689.xyz" },
-          #{ value: "https://subapi.fxxk.dedyn.io" },
-          #{ value: "https://url.v1.mk" },
-          #{ value: "https://sub.d1.mk" },
-          #{ value: "https://www.nameless13.com" },
-          #{ value: "https://sub.xeton.dev" },
-          #{ value: "https://api.wcc.best" },
         ],
         remoteConfig: [
           {
@@ -1310,4 +1298,5 @@ export default {
   }
 };
 </script>
+
 
